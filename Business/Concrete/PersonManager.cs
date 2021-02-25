0ee269ba7d1ca988f30dad1005147f7a09654acc
Melike Gökdemir <melikegkdemr@outlook.com>
2021-02-25 19:30:04 +0300

    Add project files.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,32 @@
+﻿
+using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class PersonManager: IApplicantService
+    {
+        public void ApplyForMask(Person person)
+        {
+
+        }
+
+        public List<Person> GetList()
+        {
+            return null; 
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+            
+
+
+        }
+    }
+}
---
+++ WorkAraound/Program.cs
@@ -0,0 +1,47 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace WorkAraound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person1 = new Person();
+            person1.FirstName = "ENGİN";
+            person1.LastName = "DEMİROĞ";
+            person1.DateOfYear = 1985;
+            person1.NationalIdentity = 123;
+
+
+            //SelamVer(isim: "Engin");
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+
+
+
+
+        static void SelamVer(string isim)
+        {
+            Console.WriteLine("Merhaba " + isim);
+        }
+
+
+        static int Topla(int sayi1, int sayi2)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine();
+            return sonuc;
+
+        }
+
+
+
+
+    }
+}
